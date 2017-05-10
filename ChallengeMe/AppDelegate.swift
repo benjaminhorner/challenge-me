d5ad775850d83d248450e585d9fcbacb3adb6e19
Benjamin Horner <b.e.horner@gmail.com>
2017-05-10 11:50:42 +0200

    Init firebase

--- ChallengeMe/AppDelegate.swift
+++ ChallengeMe/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    FirebaseApp.configure()
+    FIRApp.configure()
     
     return true
   }
